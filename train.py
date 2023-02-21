10456c53bdacba3fde26b9b8bf3dd8968e633e6b
Fangjun Zhou <fzhou48@wisc.edu>
2023-02-21 07:14:40 -0600

    Update: train.py Supports transfer learning.

--- train.py
+++ train.py
@@ -3,8 +3,10 @@ import yaml
 import os
 import torch
 import torch.nn as nn
+from torchtext.vocab import Vocab
+from sklearn.model_selection import train_test_split
 
-from utils.dataloader import get_dataloader_and_vocab
+from utils.dataloader import get_dataloader_and_vocab, get_custom_dataloader_and_vocab
 from utils.trainer import Trainer
 from utils.helper import (
     get_model_class,
@@ -15,28 +17,59 @@ from utils.helper import (
 )
 
 
-def train(config):
+def train(
+    config,
+    data_iter=None,
+    vocab: Vocab=None,
+    transfer_model: nn.Module=None
+):
+    # Check current device.
+    if (config["use_cuda"] and torch.cuda.is_available()):
+        device = torch.device("cuda")
+    else:
+        device = torch.device("cpu")
+    
+    print("Using device:", device)
+    
     os.makedirs(config["model_dir"])
     
-    train_dataloader, vocab = get_dataloader_and_vocab(
-        model_name=config["model_name"],
-        ds_name=config["dataset"],
-        ds_type="train",
-        data_dir=config["data_dir"],
-        batch_size=config["train_batch_size"],
-        shuffle=config["shuffle"],
-        vocab=None,
-    )
+    if data_iter is None:
+        train_dataloader, vocab = get_dataloader_and_vocab(
+            model_name=config["model_name"],
+            ds_name=config["dataset"],
+            ds_type="train",
+            data_dir=config["data_dir"],
+            batch_size=config["train_batch_size"],
+            shuffle=config["shuffle"],
+            vocab=None,
+        )
 
-    val_dataloader, _ = get_dataloader_and_vocab(
-        model_name=config["model_name"],
-        ds_name=config["dataset"],
-        ds_type="valid",
-        data_dir=config["data_dir"],
-        batch_size=config["val_batch_size"],
-        shuffle=config["shuffle"],
-        vocab=vocab,
-    )
+        val_dataloader, _ = get_dataloader_and_vocab(
+            model_name=config["model_name"],
+            ds_name=config["dataset"],
+            ds_type="valid",
+            data_dir=config["data_dir"],
+            batch_size=config["val_batch_size"],
+            shuffle=config["shuffle"],
+            vocab=vocab,
+        )
+    else:
+        # Split the dataset into train and validation sets.
+        train_dataset, val_dataset = train_test_split(data_iter, test_size=0.2)
+        train_dataloader, vocab = get_custom_dataloader_and_vocab(
+            model_name=config["model_name"],
+            data_iter=train_dataset,
+            batch_size=config["train_batch_size"],
+            shuffle=config["shuffle"],
+            vocab=vocab
+        )
+        val_dataloader, _ = get_custom_dataloader_and_vocab(
+            model_name=config["model_name"],
+            data_iter=val_dataset,
+            batch_size=config["val_batch_size"],
+            shuffle=config["shuffle"],
+            vocab=vocab
+        )
 
     vocab_size = len(vocab.get_stoi())
     print(f"Vocabulary size: {vocab_size}")
@@ -48,8 +81,23 @@ def train(config):
     optimizer_class = get_optimizer_class(config["optimizer"])
     optimizer = optimizer_class(model.parameters(), lr=config["learning_rate"])
     lr_scheduler = get_lr_scheduler(optimizer, config["epochs"], verbose=True)
-
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    
+    # If transfer learning is enabled, load the pre-trained model.
+    if transfer_model:
+        weight = model.embeddings.weight.detach().numpy()
+        transfer_weight = transfer_model["embeddings.weight"].numpy()
+        # Check if the vocabulary of transfer model is larger than the current model.
+        if transfer_weight.shape[0] > weight.shape[0]:
+            # If so, use the first part of the transfer model's vocabulary.
+            weight = transfer_weight[:weight.shape[0], :]
+        else:
+            # Otherwise, use the first part of the current model's vocabulary.
+            weight[:transfer_weight.shape[0], :] = transfer_weight
+        # Load the weight to the current model.
+        with torch.no_grad():
+            model.embeddings.weight.copy_(torch.from_numpy(weight))
+        print("Transfer learning enabled. Pre-trained model loaded.")
+        
 
     trainer = Trainer(
         model=model,
