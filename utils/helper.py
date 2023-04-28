18f9ced34db88d5081ebc5f1ccdfa095331906eb
Fangjun Zhou <fangjun.zhou02@gmail.com>
2023-04-27 22:35:24 -0500

    Removed getModelClass and getOptimizerClass method from helper

--- utils/helper.py
+++ utils/helper.py
@@ -1,54 +1,51 @@
 import os
-import yaml
+
 import torch
+
 import torch.optim as optim
 from torch.optim.lr_scheduler import LambdaLR
 
-from utils.model import CBOW_Model, SkipGram_Model
+from torchtext.vocab import Vocab
 
 
-def get_model_class(model_name: str):
-    if model_name == "cbow":
-        return CBOW_Model
-    elif model_name == "skipgram":
-        return SkipGram_Model
-    else:
-        raise ValueError("Choose model_name from: cbow, skipgram")
-        return
-
-
-def get_optimizer_class(name: str):
-    if name == "Adam":
-        return optim.Adam
-    else:
-        raise ValueError("Choose optimizer from: Adam")
-        return
-
-
-def get_lr_scheduler(optimizer, total_epochs: int, verbose: bool = True):
+def getLrScheduler(optimizer: optim.Optimizer, total_epochs: int, verbose: bool = True):
     """
     Scheduler to linearly decrease learning rate, 
     so thatlearning rate after the last epoch is 0.
+
+    Args:
+        optimizer: the optimizer used for the training.
+        total_epochs: the total number of epochs.
+        verbose: verbose enable.
+
+    Returns: LambdaLR scheduler.
+
     """
-    def lr_lambda(epoch): return (total_epochs - epoch) / total_epochs
-    lr_scheduler = LambdaLR(optimizer, lr_lambda=lr_lambda, verbose=verbose)
-    return lr_scheduler
+    def lrLambda(epoch): return (total_epochs - epoch) / total_epochs
+    lrScheduler = LambdaLR(optimizer, lr_lambda=lrLambda, verbose=verbose)
+    return lrScheduler
 
 
-def save_config(config: dict, model_dir: str):
-    """Save config file to `model_dir` directory"""
-    config_path = os.path.join(model_dir, "config.yaml")
-    with open(config_path, "w") as stream:
-        yaml.dump(config, stream)
+def saveVocab(vocab: Vocab, modelDir: str):
+    """Save vocab file to `model_dir` directory.
+
+    Args:
+        vocab: the vocab to save.
+        model_dir: the directory to store the vocab.
+    """
+    vocabPath = os.path.join(modelDir, "vocab.pt")
+    torch.save(vocab, vocabPath)
 
 
-def save_vocab(vocab, model_dir: str):
-    """Save vocab file to `model_dir` directory"""
-    vocab_path = os.path.join(model_dir, "vocab.pt")
-    torch.save(vocab, vocab_path)
+def loadVocab(modelDir: str):
+    """Load vocab file from `model_dir` directory.
 
+    Args:
+        model_dir: the directory to store the vocab.
 
-def load_vocab(path: str):
+    Returns: Loaded vocab.
+
+    """
     """Load vocab file from `model_dir` directory"""
-    vocab = torch.load(path)
+    vocab = torch.load(os.path.join(modelDir, "vocab.pt"))
     return vocab
