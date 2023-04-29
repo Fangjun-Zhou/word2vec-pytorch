501b1ce66b66f2be1105a28acb9e5938f0ef83ee
Fangjun Zhou <fangjun.zhou02@gmail.com>
2023-04-28 21:38:20 -0500

    Replace the constants with ModelConfig class

--- utils/config.py
+++ utils/config.py
@@ -7,8 +7,13 @@ class ModelConfig:
         embed_dim=256,
         embed_max_norm=1,
     ):
+        # Number of words include in the context.
         self.n_words = n_words
+        # The minimum word frequency to build the vocab.
         self.min_word_freq = min_word_freq
+        # The max length of a sequence to preprocess the dataset.
         self.max_seq_length = max_seq_length
+        # The embedded word vector dimension.
         self.embed_dim = embed_dim
+        # The max norm of the embedded vector.
         self.embed_max_norm = embed_max_norm
